5d1f07b074c63378a867ac77eecb790d524b8f04
DashaPrincess <61591826+DashaPrincess@users.noreply.github.com>
2022-06-24 22:53:35 +0300

    Добавила задачу "Даешь квадрат"

---
+++ homeworks/square/square/square.cpp
@@ -0,0 +1,105 @@
+﻿/*
+11.3. Даешь квадрат (6)
+Ломаная без самопересечений и самокасаний разделяет четырехугольник на две части, которые не лежат одна внутри другой. Каждая часть представляет собой многоугольник и задается отдельно путем перечисления координат вершин. Определить, является ли исходный четырехугольник квадратом.
+Ввод из файла INPUT.TXT. В первой строке находится число M, задающее количество вершин первого многоугольника. Следующие M строк содержат пары целых чисел - координаты точек (Xi, Yi). Если соединить точки в данном порядке, а также первую и последнюю точки, то получится первый многоугольник. Далее аналогично указываются число N – количество вершин второго многоугольника и N строк с координатами его вершин. Таким образом, тест занимает M + N + 2 строк. Начальная вершина и направление обхода вершин каждого многоугольника могут быть произвольными. 
+Вывод в файл OUTPUT.TXT. Выводится единственная строка со значением Yes или No – является четырехугольник квадратом или нет. 
+Ограничения: 3 ≤ M, N ≤ 1000;  -100 ≤ Xi, Yi ≤ 100.
+Примеры 
+Ввод 1     Ввод 2
+6          8
+0 0        0 0
+0 1        0 6
+1 1        5 5
+1 2        5 0
+2 2        3 0
+2 0        4 1
+4          1 1
+0 2        2 0
+1 2        4
+1 1        1 1
+0 1        2 0
+           3 0
+           4 1
+Вывод 1    Вывод 2
+Yes        No
+
+Куликова Дарья. ПС-22. Делала в Visual Studio 2019.
+*/                                                 
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <map>
+
+using namespace std;
+
+void ReadPoints(int m, std::ifstream& inputFile, std::set<std::pair<int, int>>& pointSet);
+int GetDoubleLength(pair<int, int>& point1, pair<int, int>& point2);
+bool IsSquare(std::set<std::pair<int, int>>& pointSet);
+
+int main()
+{
+	ifstream inputFile("input.txt");
+	ofstream outputFile("output.txt");
+	int m, n;
+	set<pair<int, int>> pointSet;
+	inputFile >> m;
+	ReadPoints(m, inputFile, pointSet);
+	inputFile >> n;
+	ReadPoints(n, inputFile, pointSet);
+
+
+	/*for (auto& it : pointSet)
+	{
+		cout << it.first << " " << it.second << endl;
+	}*/
+	if (IsSquare(pointSet))
+		outputFile << "Yes";
+	else
+		outputFile << "No";
+	return 0;
+}
+
+void ReadPoints(int m, std::ifstream& inputFile,std::set<std::pair<int, int>>& pointSet)
+{
+	int x, y;
+	for (int i = 0; i < m; i++)
+	{
+		inputFile >> x >> y;
+		pair<int, int> point{ x, y };
+		if (pointSet.find(point) == pointSet.end())
+			pointSet.insert(point);
+		else
+			pointSet.erase(point);
+	}
+}
+
+bool IsSquare(std::set<std::pair<int, int>>& pointSet)
+{
+	map<int, int> dublicateCounters;
+	for (auto point1 : pointSet)
+		for (auto point2 : pointSet)
+			if (point1 != point2)
+			{
+				int length = GetDoubleLength(point1, point2);
+				
+				if (dublicateCounters.find(length) != dublicateCounters.end())
+					dublicateCounters[length] = dublicateCounters[length] + 1;
+				else
+					dublicateCounters.insert(make_pair(length, 1));
+			}
+	for (auto& lengthCount : dublicateCounters)
+		if (lengthCount.second != 8 && lengthCount.second != 4) 
+			return false;
+	return dublicateCounters.size() == 2;
+}
+
+int GetDoubleLength(pair<int, int>& point1, pair<int, int>& point2)
+{
+	return (point2.first - point1.first) * (point2.first - point1.first) + (point2.second - point1.second) * (point2.second - point1.second);
+}
+
+
+
+
---
+++ homeworks/substraction/substraction/substraction.cpp
@@ -0,0 +1,59 @@
+﻿/*
+19.24. Длинная разность (4)
+Заданы два целых положительных числа. Требуется найти их разность. Запрещается использование библиотек длинной арифметики.
+Ввод. В первых двух строках файла INPUT.TXT содержатся  два  целых  числа  M  и  N  (1 ≤ N < M  ≤ 10200).
+Вывод. В единственной строке выходного файла OUTPUT.TXT необходимо вывести разность M – N.
+Примеры
+Ввод 1                   Ввод 2
+123456787654321          1000000000000000
+35                       1
+Вывод 1                  Вывод 2
+123456787654286          999999999999999
+
+Куликова Дарья. ПС-22. Делала в Visual Studio.
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+string SubstractNumber(string& firstNumber, string& secondNumber);
+
+int main()
+{
+    ifstream inputFile("input.txt");
+    ofstream outputFile("output.txt");
+    string firstNumber, secondNumber;
+    inputFile >> firstNumber >> secondNumber;
+    outputFile << SubstractNumber(firstNumber, secondNumber);
+    return 0;
+}
+
+string SubstractNumber(string& firstNumber, string& secondNumber)
+{
+    string result = "";
+    int borrowDigit = 0;
+    int j = secondNumber.size() - 1;
+    for (int i = firstNumber.size() - 1; i >= 0; i--)
+    {
+        int tmpDigit = (firstNumber[i] - '0') - (j >= 0 ? (secondNumber[j] - '0') : 0)  - borrowDigit;
+        if (tmpDigit >= 0)
+        {
+            if (i != 0 || tmpDigit != 0)
+                result += to_string(tmpDigit);
+            borrowDigit = 0;
+        }
+        else
+        {
+            result += to_string(tmpDigit + 10);
+            borrowDigit = 1;
+        }
+        j--;
+    }
+    reverse(result.begin(), result.end());
+    return result;
+}
+
